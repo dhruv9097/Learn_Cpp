b16748960ea6f9ad894168caa114abf2396826c4
Dhruv <singh.dhruv@gmail.com>
2023-11-22 01:00:18 +0530

    first commit

---
+++ cpp files/adding.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int num1, num2, sum;
+
+    cout<<"enter num1 ",cin>>num1;
+    cout<<"enter num2 ",cin>>num2;
+    sum = num1 + num2;
+    cout<<"sum is ",cout<<sum;
+
+    return 0;
+
+}
