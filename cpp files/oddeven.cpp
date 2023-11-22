2190434a0d39ac1f7b282c85d3e53f1031075c5b
Dhruv <singh.dhruv@gmail.com>
2023-11-22 10:06:35 +0530

    if else statements

---
+++ cpp files/oddeven.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+
+    cout<<"enter a num: ",cin>>a;
+
+    if(a%2==0){
+        cout<<"num is even."<<endl;
+    }
+    else{
+        cout<<"num is odd."<<endl;
+    }
+
+    return 0;
+}
